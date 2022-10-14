779cd321051b3a2c2f5cd7069ccb2ff70e38299c
vamsisai9 <vamsi.sai.work@gmail.com>
2022-10-14 13:20:31 +0530

    adding calculateStats

--- statistics.py
+++ statistics.py
@@ -1,3 +1,13 @@
 
 def calculateStats(numbers):
-  return None
+  if len(numbers)>0:
+    numAvg = sum(numbers)/len(numbers)
+    numMin = min(numbers)
+    numMax = max(numbers)
+
+  statData = {}
+  statData['avg'] = numAvg
+  statData['max'] = numMax
+  statData['min'] = numMin
+  
+  return statData
--- statistics.test.py
+++ statistics.test.py
@@ -1,6 +1,8 @@
 import unittest
 import statistics
-
+class EmailAlert:
+  def __init__(self):
+    self.emailAlert = True
 class StatsTest(unittest.TestCase):
   def test_report_min_max_avg(self):
     computedStats = statistics.calculateStats([1.5, 8.9, 3.2, 4.5])
